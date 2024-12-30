abc1c4223948a3cf94a5b3ea3c5a35b2bb15d8a9
kacperw02 <kropczakk@gmail.com>
2024-12-30 20:22:11 +0100

    Dodaj pliki projektów.

---
+++ Projekt bazy/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Projekt_bazy.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
